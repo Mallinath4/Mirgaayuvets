15896cc3e420c7048a5671c7275ffdf539b51f26
Mallinath Khonde <115429767+Mallinath4@users.noreply.github.com>
2025-08-26 22:15:59 +0530

    Update app.py
    Signed-off-by: Mallinath Khonde <115429767+Mallinath4@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for
-
+import os
 app = Flask(__name__)
 
 # Home page
@@ -667,5 +667,7 @@ def about():
 def contact():
     return render_template('contact.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
